d213edf25ba76b3c6a06733161287c9990a0c38d
Audrey1656 <33307688+Audrey1656@users.noreply.github.com>
2019-10-18 19:29:29 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -22,7 +22,7 @@ request = pc.makeRequestRSpec()
 
 # Only Ubuntu images supported.
 imageList = [
-    ('urn:publicid:IDN+clemson.cloudlab.us+image+cops-PG0:webcachesim_simulation:1', 'WEBCACHESIM_SNAPSHOT'),
+    ('urn:publicid:IDN+emulab.net+image+cops:wcs-test', 'WEBCACHESIM_SNAPSHOT'),
     ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD', 'UBUNTU 18.04'),
     ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU16-64-STD', 'UBUNTU 16.04'),
     ('urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU14-64-STD', 'UBUNTU 14.04'),
@@ -44,7 +44,7 @@ pc.defineParameter("osImage", "Select OS image",
 
 pc.defineParameter("DATASET", "URN of your dataset dataset", 
                    portal.ParameterType.STRING,
-                   "urn:publicid:IDN+clemson.cloudlab.us:cops-pg0+stdataset+webcachesim_short_term_256GB")
+                   "urn:publicid:IDN+emulab.net:cops+stdataset+lrb-256")
 
 # Always need this when using parameters
 params = pc.bindParameters()
