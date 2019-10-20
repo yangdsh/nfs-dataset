f30e4aea683e0744e95d2344f52f18ad3054d82d
Audrey1656 <33307688+Audrey1656@users.noreply.github.com>
2019-10-19 21:57:28 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -84,7 +84,7 @@ for i in range(1, params.clientCount+1):
     nfsLan.addInterface(node.addInterface())
     # Initialization script for the clients
     node.addService(pg.Execute(shell="sh", command="sudo /bin/bash /local/repository/nfs-client.sh"))
-    node.addService(pg.Execute(shell="sh", command="sudo /bin/cp /local/repository/.bashrc /users/zhenyus/"))
+    node.addService(pg.Execute(shell="sh", command="sudo /bin/cp /local/repository/.bashrc /users/audreyc/"))
     pass
 
 # Print the RSpec to the enclosing page.
