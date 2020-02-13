9ae0d5180c2618945995779ea6f9874b8492ed08
Audrey1656 <33307688+Audrey1656@users.noreply.github.com>
2020-02-13 14:44:46 -0500

    Update profile.py

--- profile.py
+++ profile.py
@@ -44,7 +44,7 @@ pc.defineParameter("osImage", "Select OS image",
 
 pc.defineParameter("DATASET", "URN of your dataset dataset", 
                    portal.ParameterType.STRING,
-                   "urn:publicid:IDN+clemson.cloudlab.us:cops-pg0+stdataset+storage-5")
+                   "urn:publicid:IDN+clemson.cloudlab.us:cops-pg0+stdataset+storage-6")
 
 # Always need this when using parameters
 params = pc.bindParameters()
